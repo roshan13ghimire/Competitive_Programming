3b2a987c0ea2b59a58b9405f6a194458238c6bfc
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2020-07-22 21:44:10 +0545

    Add files via upload

---
+++ CodeChef/CodeChef_Solution_In_Python/Chef_and_Interactive_Contests.py
@@ -0,0 +1,9 @@
+#Chef_and_Interactive_Contests
+n , r = map(int,input().split())
+for i in range(n):
+    a = int(input())
+    if a >= r:
+        print("Good boi")
+    else:
+        print("Bad boi")
+    
