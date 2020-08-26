be48747809013ea2919b5808c4a60d40ec449624
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2020-08-26 20:20:46 +0545

    Add files via upload

---
+++ HackerRank/Alphabet_Rangoli.py
@@ -0,0 +1,12 @@
+#Alphabet_Rangoli
+def print_rangoli(size):
+    myStr = 'abcdefghijklmnopqrstuvwxyz'[0:size]
+    for i in range(size-1, -size, -1):
+        x = abs(i)
+        if x >= 0:
+            line = myStr[size:x:-1]+myStr[x:size]
+            print ("--"*x+ '-'.join(line)+"--"*x)
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
---
+++ HackerRank/Capitalize!.py
@@ -0,0 +1,20 @@
+#Capitalize!
+
+import sys
+def solve(s):
+   s = s.split(" ")
+   return(" ".join(i.capitalize() for i in s))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+    
