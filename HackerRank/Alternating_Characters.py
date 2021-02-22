6b2f7206bdec82bb11aa10a3c81d77b8a3609eed
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-02-22 08:43:25 +0530

    Update Alternating_Characters.py

--- HackerRank/Alternating_Characters.py
+++ HackerRank/Alternating_Characters.py
@@ -1,8 +1,8 @@
 #Alternating_Characters
 for _ in range(int(input())):
     s=input()
-    c=0
+    d=0
     for i in range(1,len(s)):
         if(s[i-1]==s[i]):
-            c+=1
-    print(c)        
+            d+=1
+    print(d)        
