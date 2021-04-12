0c238a391c6acee8763968ef298b765c133b7111
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-04-12 08:30:05 +0530

    Update Ali_and_Helping_innocent_people.py

--- HackerEarth/Ali_and_Helping_innocent_people.py
+++ HackerEarth/Ali_and_Helping_innocent_people.py
@@ -6,6 +6,6 @@ f3 = (int(s[0])+int(s[1]))%2==0
 f4 = (int(s[3])+int(s[4]))%2==0
 f5 = (int(s[4])+int(s[5]))%2==0
 if(f1 and f2 and f3 and f4 and f5):
-    print('valid')
+    print('Valid')
 else:
-    print('invalid')
+    print('Invalid')
